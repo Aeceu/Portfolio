77c1d7be7c022b9bdfccac0b52230ad846a2d1d5
kaneki081 <joseacebuche2@gmail.com>
2023-08-15 00:53:15 +0800

    commit

--- components/animate/appear.tsx
+++ components/animate/appear.tsx
@@ -1,37 +1,37 @@
-'use client'
-import React, { useEffect } from 'react';
-import {motion,useInView,useAnimation  } from 'framer-motion';
-import { useRef } from 'react';
+"use client";
+import React, { useEffect } from "react";
+import { motion, useInView, useAnimation } from "framer-motion";
+import { useRef } from "react";
 
-interface Props{
-    children:JSX.Element | JSX.Element[],
-    className?:string,
-    delay?:number
+interface Props {
+  children: JSX.Element | JSX.Element[];
+  className?: string;
+  delay?: number;
 }
 
-export default function Appear({delay,children,className}:Props) {
+export default function Appear({ delay, children, className }: Props) {
   const ref = useRef(null);
-  const isInView = useInView(ref,{once:true});
+  const isInView = useInView(ref, { once: true });
   const mainControls = useAnimation();
-  
-  useEffect(()=>{
-    if(isInView){
-      mainControls.start("visible")
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
     }
-  },[isInView])
+  }, [isInView, mainControls]);
 
   return (
     <motion.div
       ref={ref}
       className={className}
       variants={{
-        hidden:{opacity:0,y:-75},
-        visible:{opacity:1,y:0,transition:{delay:delay ? delay : 0}}
+        hidden: { opacity: 0, y: -75 },
+        visible: { opacity: 1, y: 0, transition: { delay: delay ? delay : 0 } },
       }}
       initial="hidden"
       animate={mainControls}
-      >
+    >
       {children}
     </motion.div>
-  )
+  );
 }
