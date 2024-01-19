09db8b84f577608526bb37d96d2be352e9113a01
kaneki081 <joseacebuche2@gmail.com>
2024-01-19 09:26:19 +0800

    bubble sort added!

--- components/pages/Projects.tsx
+++ components/pages/Projects.tsx
@@ -25,7 +25,7 @@ export default function Projectspage() {
         <Divider />
       </div>
 
-      <div className="grid grid-cols-2 place-items-center w-full h-full">
+      <div className="grid grid-cols-2 gap-4 place-items-center w-full h-full">
         {/* Travel Wander */}
         <div className="z-50 relative w-[600px] h-[300px] overflow-hidden flex flex-col rounded-md bg-[url(/travel-wander/2.png)] bg-cover bg-center">
           <div
@@ -70,6 +70,33 @@ export default function Projectspage() {
               target="_blank"
               passHref
               className="text-xs px-2 py-1 rounded-sm bg-slate-700 w-max hover:text-emerald-500 text-white"
+            >
+              Live view
+            </Link>
+          </div>
+        </div>
+
+        {/* Bubble Sort Visualizer */}
+        <div className="z-50 relative w-[600px] h-[300px] overflow-hidden flex flex-col rounded-md bg-[url(/bubble-sort/1.png)] bg-cover bg-center">
+          <div
+            className="hover:-bottom-0 transition-all duration-300
+        absolute -bottom-20
+        w-full  rounded-t-md shadow-xl bg-slate-900 flex flex-col p-2  "
+          >
+            <h1 className="text-xs text-white/70">
+              Data Structure Application
+            </h1>
+            <h1 className="text-amber-500">Bubble Sort Visualizer</h1>
+            <p className="mb-6  text-white/70">
+              A application about bubble sort algorithm and visualize it to see
+              how it works.
+            </p>
+            <Link
+              href="https://visualsortcraft.vercel.app/"
+              rel="noopener noreferrer"
+              target="_blank"
+              passHref
+              className="text-xs px-2 py-1 rounded-sm bg-slate-700 w-max hover:text-amber-500 text-white"
             >
               Live view
             </Link>
