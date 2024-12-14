90c9bbdb8390492577c3958f949d63d405b4bda5
kaneki081 <joseacebuche2@gmail.com>
2024-12-14 13:45:24 +0800

    changes button layout and fix contact overlay

--- components/ProjectFolder.tsx
+++ components/ProjectFolder.tsx
@@ -5,79 +5,81 @@ import Badge from "./Badge";
 import Image from "next/image";
 
 type Props = {
-  tecnologies: string[];
-  projectImg: string;
-  name: string;
-  description: string;
-  projectIndex: string;
-  liveUrl: string;
-  githubUrl: string;
+	tecnologies: string[];
+	projectImg: string;
+	name: string;
+	description: string;
+	projectIndex: string;
+	liveUrl: string;
+	githubUrl: string;
 };
 
 const ProjectFolder: FC<Props> = ({
-  githubUrl,
-  liveUrl,
-  description,
-  name,
-  projectImg,
-  tecnologies,
-  projectIndex,
+	githubUrl,
+	liveUrl,
+	description,
+	name,
+	projectImg,
+	tecnologies,
+	projectIndex,
 }) => {
-  return (
-    <div className={`group relative flex flex-col items-center hover:z-50 `}>
-      <div className="relative hover:scale-105 cursor-pointer w-[300px] h-[200px] md:w-[500px] md:h-[300px] bg-emerald-500 rounded-b-lg rounded-r-lg z-[10] p-2 flex flex-col items-center dark:shadow-white-md shadow-slate-md transition-all duration-500">
-        <div
-          className="border-r-[20px]   border-emerald-500 absolute  bg-emerald-500  text-white p-2 rounded-t-md -top-8 left-0 leading-tight"
-          style={{
-            clipPath: "polygon(0 0, calc(100% - 30px) 0, 100% 100%, 0 100%)",
-          }}>
-          Project #{projectIndex}
-        </div>
-        <Image
-          src={projectImg}
-          width={500}
-          height={300}
-          alt={projectImg.split("/")[1]}
-          className="h-full rounded-lg  object-cover"
-        />
-      </div>
-      <div
-        className=" 
-      w-[calc(300px-16px)] md:w-[calc(500px-16px)] 
-      md:group-hover:animate-appearMD group-hover:animate-appearSM 
+	return (
+		<div className={`group relative flex flex-col items-center hover:z-[100] `}>
+			<div className="relative hover:scale-105 cursor-pointer w-[300px] h-[200px] md:w-[500px] md:h-[300px] bg-emerald-500 rounded-b-lg rounded-r-lg z-[10] p-2 flex flex-col items-center dark:shadow-white-md shadow-slate-md transition-all duration-500">
+				<div
+					className="border-r-[20px]   border-emerald-500 absolute  bg-emerald-500  text-white p-2 rounded-t-md -top-8 left-0 leading-tight"
+					style={{
+						clipPath: "polygon(0 0, calc(100% - 30px) 0, 100% 100%, 0 100%)",
+					}}>
+					Project #{projectIndex}
+				</div>
+				<Image
+					src={projectImg}
+					width={500}
+					height={300}
+					alt={projectImg.split("/")[1]}
+					className="h-full rounded-lg  object-cover"
+				/>
+			</div>
+			<div
+				className="
+      w-[calc(300px-16px)] md:w-[calc(500px-16px)]
+      md:group-hover:animate-appearMD group-hover:animate-appearSM
       h-full md:min-h-[250px] md:h-full
       flex flex-col justify-between p-2 md:p-4 bg-emerald-500  rounded-b-lg z-[5] absolute gap-2 dark:shadow-white-md shadow-slate-md">
-        <span className="leading-3 mt-4">
-          <h1 className="font-extrabold text-xs md:text-lg text-white tracking-widest">{name}</h1>
-          <p className="text-xs md:text-sm  text-white">{description}</p>
-        </span>
-        <div className="md:my-2 flex items-center gap-2 leading-3 ">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href={liveUrl}
-            className="w-max flex items-center gap-2 text-xs bg-white text-slate-950 px-2 py-1 md:px-3 md:py-1.5 font-extrabold shadow-slate-sm  rounded-sm ">
-            Live
-            <LucideEarth className="md:w-4 md:h-4 w-2 h-2" />
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href={githubUrl}
-            className="w-max flex items-center gap-2 text-xs bg-white text-slate-950 px-2 py-1 md:px-3 md:py-1.5 font-extrabold shadow-slate-sm  rounded-sm ">
-            Github <Github className="md:w-4 md:h-4 w-2 h-2" />
-          </a>
-        </div>
-        <div className="hidden md:flex flex-wrap gap-2">
-          <p className="text-white border-b text-lg font-extrabold tracking-widest w-full">
-            Technologies
-          </p>
-          {tecnologies.map((item, i) => (
-            <Badge key={i} text={item} />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+				<span className="leading-3 mt-4">
+					<h1 className="font-extrabold text-xs md:text-lg text-white tracking-widest">
+						{name}
+					</h1>
+					<p className="text-xs md:text-sm  text-white">{description}</p>
+				</span>
+				<div className="md:my-2 flex items-center gap-2 leading-3 ">
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href={liveUrl}
+						className="w-max flex items-center gap-2 text-xs bg-white text-slate-950 px-2 py-1 md:px-3 md:py-1.5 font-extrabold shadow-slate-sm  rounded-sm ">
+						Live
+						<LucideEarth className="md:w-4 md:h-4 w-2 h-2" />
+					</a>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href={githubUrl}
+						className="w-max flex items-center gap-2 text-xs bg-white text-slate-950 px-2 py-1 md:px-3 md:py-1.5 font-extrabold shadow-slate-sm  rounded-sm ">
+						Github <Github className="md:w-4 md:h-4 w-2 h-2" />
+					</a>
+				</div>
+				<div className="hidden md:flex flex-wrap gap-2">
+					<p className="text-white border-b text-lg font-extrabold tracking-widest w-full">
+						Technologies
+					</p>
+					{tecnologies.map((item, i) => (
+						<Badge key={i} text={item} />
+					))}
+				</div>
+			</div>
+		</div>
+	);
 };
 export default ProjectFolder;
--- components/pages/Contact.tsx
+++ components/pages/Contact.tsx
@@ -6,110 +6,114 @@ import axios from "axios";
 import { Loader2 } from "lucide-react";
 
 interface FormData {
-  name: string;
-  email: string;
-  message: string;
+	name: string;
+	email: string;
+	message: string;
 }
 
 const Contact = () => {
-  const [loading, setLoading] = useState(false);
-  const [formData, setFormData] = useState<FormData>({
-    name: "",
-    email: "",
-    message: "",
-  });
+	const [loading, setLoading] = useState(false);
+	const [formData, setFormData] = useState<FormData>({
+		name: "",
+		email: "",
+		message: "",
+	});
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
+	const handleChange = (
+		e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+	) => {
+		const { name, value } = e.target;
+		setFormData((prev) => ({
+			...prev,
+			[name]: value,
+		}));
+	};
 
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
+	const handleSubmit = async (e: FormEvent) => {
+		e.preventDefault();
 
-    setLoading(true);
-    try {
-      const response = await axios.post("/api/email", formData);
-      alert(response.data.message);
-    } catch (error) {
-      alert("Error sending email");
-    } finally {
-      setLoading(false);
-    }
-  };
+		setLoading(true);
+		try {
+			const response = await axios.post("/api/email", formData);
+			alert(response.data.message);
+		} catch (error) {
+			alert("Error sending email");
+		} finally {
+			setLoading(false);
+		}
+	};
 
-  return (
-    <div id="contact-page" className="relative min-h-[700px] flex flex-col items-center p-4 gap-8">
-      <div className="w-full flex items-center">
-        <DelayOnReveal
-          delay={0.2}
-          className="relative pr-4 flex text-5xl md:text-7xl lg:text-8xl font-extrabold text-emerald-500 mb-4 z-50 
+	return (
+		<div
+			id="contact-page"
+			className="relative min-h-[700px] flex flex-col items-center p-4 gap-8">
+			<div className="w-full flex items-center">
+				<DelayOnReveal
+					delay={0.2}
+					className="relative pr-4 flex text-5xl md:text-7xl lg:text-8xl font-extrabold text-emerald-500 mb-4 z-50
           bg-white dark:bg-slate-950
           dark:[text-shadow:3px_3px_0_rgb(255_255_255_)] [text-shadow:3px_3px_0_rgb(2_6_23_)]
           lg:dark:[text-shadow:5px_5px_0_rgb(255_255_255_)] lg:[text-shadow:5px_5px_0_rgb(2_6_23_)]">
-          Contact Me
-        </DelayOnReveal>
-        <Divider />
-      </div>
+					Contact Me
+				</DelayOnReveal>
+				<Divider />
+			</div>
 
-      <div className="w-full h-full flex items-center justify-center">
-        <form
-          onSubmit={handleSubmit}
-          className="min-w-[300px] w-[500px] h-full flex flex-col items-start gap-6 p-2">
-          <h1
-            className="text-center w-full text-3xl md:text-4xl lg:text-5xl font-extrabold text-emerald-500 mb-4
+			<div className="w-full h-full flex items-center justify-center">
+				<form
+					onSubmit={handleSubmit}
+					className="min-w-[300px] w-[500px] h-full flex flex-col items-start gap-6 p-2">
+					<h1
+						className="text-center w-full text-3xl md:text-4xl lg:text-5xl font-extrabold text-emerald-500 mb-4
       dark:[text-shadow:3px_3px_0_rgb(255_255_255_)]
       [text-shadow:3px_3px_0_rgb(2_6_23_)]">
-            Let&apos;s keep in touch
-          </h1>
-          <span className="w-full flex flex-col gap-2">
-            <h1>Name</h1>
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              className="outline-none px-2 py-4 text-lg text-slate-950 border-2 border-slate-950 dark:border-white shadow-green-md"
-              placeholder="type your name"
-            />
-          </span>
-          <span className="w-full flex flex-col gap-2">
-            <h1>Email</h1>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className="outline-none px-2 py-4 text-lg text-slate-950 border-2 border-slate-950 dark:border-white shadow-green-md"
-              placeholder="type your email"
-            />
-          </span>
-          <span className="w-full flex flex-col gap-2">
-            <h1>Message</h1>
-            <textarea
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-              rows={5}
-              className="outline-none px-2 py-4 text-lg text-slate-950 border-2 border-slate-950 dark:border-white shadow-green-md"
-              placeholder="type your message here"
-            />
-          </span>
-          <DelayOnReveal className="flex gap-2 w-max items-center border-2 relative hover:animate-up-down hover:underline border-slate-950 dark:border-white shadow-green-sm dark:bg-white dark:text-slate-950">
-            <button
-              disabled={loading}
-              type="submit"
-              className="flex gap-2 md:px-6 md:py-4 px-3 py-1.5 text-sm md:text-base items-center">
-              {loading ? <Loader2 className="animate-spin" /> : "Send Message"}
-            </button>
-          </DelayOnReveal>
-        </form>
-      </div>
-    </div>
-  );
+						Let&apos;s keep in touch
+					</h1>
+					<span className="w-full flex flex-col gap-2">
+						<h1>Name</h1>
+						<input
+							type="text"
+							name="name"
+							value={formData.name}
+							onChange={handleChange}
+							className="outline-none px-2 py-4 text-lg text-slate-950 border-2 border-slate-950 dark:border-white shadow-green-md"
+							placeholder="type your name"
+						/>
+					</span>
+					<span className="w-full flex flex-col gap-2">
+						<h1>Email</h1>
+						<input
+							type="email"
+							name="email"
+							value={formData.email}
+							onChange={handleChange}
+							className="outline-none px-2 py-4 text-lg text-slate-950 border-2 border-slate-950 dark:border-white shadow-green-md"
+							placeholder="type your email"
+						/>
+					</span>
+					<span className="w-full flex flex-col gap-2">
+						<h1>Message</h1>
+						<textarea
+							name="message"
+							value={formData.message}
+							onChange={handleChange}
+							rows={5}
+							className="outline-none px-2 py-4 text-lg text-slate-950 border-2 border-slate-950 dark:border-white shadow-green-md"
+							placeholder="type your message here"
+						/>
+					</span>
+					<DelayOnReveal className="flex gap-2 w-max items-center border-2 relative hover:animate-up-down hover:underline border-slate-950 dark:border-white shadow-green-sm dark:bg-white dark:text-slate-950">
+						<button
+							disabled={loading}
+							type="submit"
+							className="flex gap-2 md:px-6 md:py-4 px-3 py-1.5 text-sm md:text-base items-center">
+							{loading ? <Loader2 className="animate-spin" /> : "Send Message"}
+						</button>
+					</DelayOnReveal>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default Contact;
